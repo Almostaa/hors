1052efe3e7863cc1050ad129f3da3ae2196d0ead
Almostaa <67301800+Almostaa@users.noreply.github.com>
2020-11-02 21:39:36 +0800

    Add files via upload

---
+++ registerration_system/src/main/java/com/sesame/interceptor/MyMvcConfig.java
@@ -0,0 +1,24 @@
+package com.sesame.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //跨域
+        registry.addInterceptor(new HttpInterceptor()).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+}
