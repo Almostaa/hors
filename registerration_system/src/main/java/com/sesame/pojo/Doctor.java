bf59a36b381cd64dfb6091b80b69311cad8e0c83
Almostaa <67301800+Almostaa@users.noreply.github.com>
2020-11-04 11:23:35 +0800

    Add files via upload

---
+++ registerration_system/src/main/java/com/sesame/pojo/Doctor.java
@@ -0,0 +1,83 @@
+package com.sesame.pojo;
+
+public class Doctor {
+    private Integer dno;
+
+    private String dname;
+
+    private String sex;
+
+    private Integer roomno;
+
+    private String rank;
+
+    private String grade;
+
+    private String describe;
+
+    private String picture;
+
+    public Integer getDno() {
+        return dno;
+    }
+
+    public void setDno(Integer dno) {
+        this.dno = dno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname == null ? null : dname.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public Integer getRoomno() {
+        return roomno;
+    }
+
+    public void setRoomno(Integer roomno) {
+        this.roomno = roomno;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank == null ? null : rank.trim();
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade == null ? null : grade.trim();
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe == null ? null : describe.trim();
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture == null ? null : picture.trim();
+    }
+}
